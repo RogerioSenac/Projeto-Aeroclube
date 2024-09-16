3be146e0900f960428bb67ef0d1611071db67805
RogerioSenac <roge.pupo.toledo@gmail.com>
2024-09-16 10:19:59 -0300

    refacture.rogerio - DashAcesso.php and Login.php negaçao

--- Maverick/senha/login.php
+++ Maverick/senha/login.php
@@ -2,40 +2,42 @@
 // login.php
 include '../conexao.php';
 
-
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = $_POST['usuario'];
-    $password = $_POST['senhaUsuario'];
-
+    $senhaUsuario = $_POST['senhaUsuario'];
+    echo ("u:$username, s:$senhaUsuario");
+    
+        
     // Chama a função loginUser
-    $user = loginUser($username, $password);
-
-    if ($user) {
+    $user = loginUsuario($username, $senhaUsuario);
+    print_r($user);
+    
+    if (!$user) {
         // Login bem-sucedido: redirecionar para o painel ou outra página
-        header("Location: index.php"); // Supondo que você tenha um dashboard.php
+        header("Location: ../index.php"); // Supondo que você tenha um dashboard.php
         exit;
     } else {
+        
         // Falha no login: redirecionar de volta para a página de login com uma mensagem de erro
         header("Location: DashAcesso.php?error=invalid_credentials");
         exit;
     }
 }
-function loginUser($username, $password)
+function loginUsuario($username, $password)
 {
     global $conexao;
 
     // Prepara a consulta para buscar o usuário pelo nome de usuário
-    $stmt = $conexao->prepare("SELECT * FROM password WHERE nomeUsuario = :username");
-    $stmt->bindParam(':username', $username);
+    $stmt = $conexao->prepare("SELECT usuario, senhaUsuario FROM acesso WHERE usuario = :usuario");
+    $stmt->bindParam(':usuario', $username);
     $stmt->execute();
 
     // Busca o resultado
-    $user = $stmt->fetch(PDO::FETCH_ASSOC);
-
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);   
     
 
     // Verifica se o usuário foi encontrado e se a senha está correta
-    if ($user && password_verify($password, $user['password'])) {
+    if ($user && password_verify($password, $user['senhaUsuario'])) {
         // Login bem-sucedido
         return $user;
     } else {
@@ -43,4 +45,5 @@ function loginUser($username, $password)
         return false;
     }
 }
+    
 ?>
