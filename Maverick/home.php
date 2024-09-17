73595bcbd74c4c6b057d6b35888307074f922169
MaxPichy <camilamachadodesouza77@gmail.com>
2024-09-17 10:02:53 -0300

    imagem 14 bis imagens nos cards e adição de asset imagem aeronaves 14 bis

--- Maverick/home.php
+++ Maverick/home.php
@@ -46,7 +46,7 @@
         <div class="row">
             <div class="col-md-4">
                 <div class="card">
-                    <img src="path-to-image1.jpg" class="card-img-top" alt="Aulas de Pilotagem">
+                    <img src="https://plus.unsplash.com/premium_photo-1661964062531-dcbd3b63d1bd?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="card-img-top" alt="Aulas de Pilotagem">
                     <div class="card-body">
                         <h5 class="card-title">Aulas de Pilotagem</h5>
                         <p class="card-text">Aprenda a voar com os melhores instrutores do país. Cursos para iniciantes e avançados.</p>
@@ -56,7 +56,7 @@
             </div>
             <div class="col-md-4">
                 <div class="card">
-                    <img src="path-to-image2.jpg" class="card-img-top" alt="Eventos">
+                    <img src="https://images.unsplash.com/photo-1589042445387-f9099ff05f8e?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="card-img-top" alt="Eventos">
                     <div class="card-body">
                         <h5 class="card-title">Eventos</h5>
                         <p class="card-text">Participe dos nossos eventos exclusivos para membros e visitantes, com voos panorâmicos e mais.</p>
@@ -66,7 +66,7 @@
             </div>
             <div class="col-md-4">
                 <div class="card">
-                    <img src="path-to-image3.jpg" class="card-img-top" alt="Associe-se">
+                    <img src="https://images.unsplash.com/photo-1563540340459-16201ef69a8d?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="card-img-top" alt="Associe-se">
                     <div class="card-body">
                         <h5 class="card-title">Associe-se</h5>
                         <p class="card-text">Torne-se um membro do nosso aeroclube e aproveite benefícios exclusivos e descontos.</p>
