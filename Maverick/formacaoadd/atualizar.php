c6d99953d3586566dbd8cecc9807810df958f5d9
rogeriosenac <roge.pupo.toledo@gmail.com>
2024-09-20 22:34:35 -0300

    refacture.rogerio - FormaçoesAdcionais - change Style

--- Maverick/formacaoadd/atualizar.php
+++ Maverick/formacaoadd/atualizar.php
@@ -6,7 +6,7 @@ $buscarFormacao = $conexao->prepare("SELECT * FROM formacoes_adicionais WHERE id
 $buscarFormacao->execute([$id]);
 $formacao = $buscarFormacao->fetch(PDO::FETCH_ASSOC);
 
-if($_SERVER['REQUEST_METHOD']== "POST"){
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $nome = $_POST['nomeFormacao'];
 
     $atualizar = $conexao->prepare("UPDATE formacoes_adicionais SET nomeFormacao=? WHERE idFormAdd=?");
@@ -19,11 +19,15 @@ if($_SERVER['REQUEST_METHOD']== "POST"){
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="../Assets/CSS/estilo.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <title>Escola Aviação Maverick</title>
     <style>
         .input-label-wrapper {
             display: flex;
@@ -45,9 +49,16 @@ if($_SERVER['REQUEST_METHOD']== "POST"){
     </style>
     <title>Academy Maverick - Atualizacao de Registro de Formacão</title>
 </head>
+
 <body>
+    <div class="navbar_menu">
+        <img src="../Assets/images/aeronaves/logo.png" alt="Logo">
+    </div>
+    <div class="mensagem">
+        <h1>Controle de Formações de Voos</h1>
+    </div>
     <div class="container">
-        <h1 class="my-4">Academy Maverick - Atualização de Registro de Formacão</h1>
+        
         <form method="POST">
             <div class="mb-3 input-label-wrapper">
                 <label for="nomeFormacao" class="form-label">Formação</label>
@@ -58,4 +69,5 @@ if($_SERVER['REQUEST_METHOD']== "POST"){
         </form>
     </div>
 </body>
+
 </html>
